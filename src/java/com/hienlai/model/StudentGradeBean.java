cb8babc7f41d487d453d21cd2adb7b4eb73dc81c
Jennifer <pnaiman1@jhu.edu>
2016-08-03 23:20:13 -0400

    Faculty rebased on new stuff

---
+++ src/java/com/hienlai/model/StudentGradeBean.java
@@ -0,0 +1,53 @@
+package com.hienlai.model;
+
+import java.io.Serializable;
+
+public class StudentGradeBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String fname;
+	private String lname;
+	private double grade;
+	private int enrollId;
+	
+	public StudentGradeBean(String fname, String lname, double grade, int id) {
+		this.fname = fname;
+		this.lname = lname;
+		this.grade = grade;
+		this.enrollId = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+
+	public int getId() {
+		return enrollId;
+	}
+
+	public void setId(int id) {
+		this.enrollId = id;
+	}
+	
+}
